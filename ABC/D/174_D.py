c24084123f38b171addbd26a62231387e2e18237
Daiki Murata <Daiki.Murata@ibm.com>
2020-08-03 21:18:31 +0900

    add 174_D

--- ABC/D/174_D.py
+++ ABC/D/174_D.py
@@ -1,26 +1,5 @@
 N = int(input())
 c = input()
-l = []
 
-for i in range(len(c)):
-    l.append(c[i])
-cnt = l.count('R')
-
-
-def my_index(l, x, default=False):
-    if x in l:
-        return l.index(x)
-    else:
-        return default
-
-
-for i in range(N):
-    if l[-(i+1)] == 'R':
-        if my_index(l, 'W'):
-            l[l.index('W')] = 'R'
-            l[-(i+1)] = 'W'
-            print(i, l)
-    if l.index('W') == cnt:
-        print(i+1)
-        break
-
+numR = c.count('R')
+print(0 if numR == 0 or numR == len(c) else c[:numR].count('W'))
